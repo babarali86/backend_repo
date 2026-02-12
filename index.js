d14a010c31a57af81e6fc93fb855bda9436b9f63
Dilawar Qayoum <dilawarqayoum40@gmail.com>
2026-02-12 23:23:44 +0500

    change index file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("simple welcome to our project");
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to our project' });
+});
+
+app.listen(PORT, () => {
+  console.log('simple welcome to our project');
+  console.log(`Server running on port ${PORT}`);
+});
