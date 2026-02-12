c3140bcd2a24f274f4d2cb81ce59d56df3f70fd4
Dilawar Qayoum <dilawarqayoum40@gmail.com>
2026-02-13 00:01:23 +0500

    Fix PORT and start script for Railway deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { PDFDocument } = require("pdf-lib");
 const fontkit = require("@pdf-lib/fontkit");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
